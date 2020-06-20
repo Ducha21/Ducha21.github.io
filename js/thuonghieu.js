afab458f1ffbfc1b0dad04e88da1942ed21d076f
unknown <Duc2000bn1407@gmail.com>
2020-06-19 19:50:35 -0700

    thay doi logo.css,thuonghieu.js,thuonghieu.html

--- js/thuonghieu.js
+++ js/thuonghieu.js
@@ -1,34 +1,32 @@
-var slideIndex = 0;
 //Mặc đinh nó sẽ ở cai anh đâu tiên
-showSlides();
+var slideIndex = 0;
 
-  function showSlides() {
-  var i;
-  // Phương thức trả về tập hợp các phần tử 
-  var slides = document.getElementsByClassName("mySlides");
-  //gan slide bang class "mySlides"
-  var dots = document.getElementsByClassName("dot");
-  //gan dots bang class dot
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  //vong lap for dung de ẩn tât cả cac anh di
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1} 
-  //Neu slideindex lon hon do dai slides thi gan slideindex bang 1 . Cai nay de dam bao tính xoay voòg   
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  //replace("a","b") là kieu thay chuỗi nay baằg chuôi kia
-  //replace(" active", "") cung tuơng tự vây
-  //
-  slides[slideIndex-1].style.display = "block";  
-  //Hiên thị ra anh đau tien
-  dots[slideIndex-1].className += " active";
-  // cái này là cái gì 
-  // để hiển thi vị trí dâu châm kia
-  
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-  //CÀI DAT THỜI GIAN HIỂN THị img tiep theo
+
+function showSlides() {
+    var i;
+    // Phương thức trả về tập hợp các phần tử 
+    var slides = document.getElementsByClassName("mySlides");
+    //gan slide bang class "mySlides"
+    var dots = document.getElementsByClassName("dot");
+    //gan dots bang class dot
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    //vong lap for dung de ẩn tât cả cac anh di
+    slideIndex++;
+    if (slideIndex >= slides.length) { slideIndex = 0; }
+    //Neu slideindex lon hon do dai slides thi gan slideindex bang 1 . Cai nay de dam bao tính xoay voòg   
+
+    //Hiển thị ra slide thứ
+    slides[slideIndex].style.display = "block";
+
+
+    // Change image every x seconds
+    //CÀI DAT THỜI GIAN HIỂN THị img tiep theo
+    setTimeout(showSlides, 6000);
 
 }
+
+
+//Gọi hàm showSlides để chạy các ảnh
+showSlides();
